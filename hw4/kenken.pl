2cfa9bede0ce3f544d8b5df4069f5c9e1efa4407
Zhengtong Liu <zhengtongliu@outlook.com>
2021-05-10 20:20:15 +0800

    test

--- hw4/kenken.pl
+++ hw4/kenken.pl
@@ -65,56 +65,121 @@ kenken(N, C, T) :-
     maplist(check_constrain(T), C),
     maplist(fd_labeling, X).
 
+
+% plain_all_different(L) :-
+%     length(L, Length),
+%     sort(L, L_ordered),
+%     length(L_ordered, Length_ordered),
+%     Length == Length_ordered.
+
+% plain_all_unique([]).
+% plain_all_unique([Hd|Tl]) :-
+%     member(Hd, Tl), !, fail.
+% plain_all_unique([_|Tl]) :-
+%     plain_all_unique(Tl).
+
+% plain_within_domain(_, []).
+% plain_within_domain(N, [Hd|Tl]) :-
+%     length(Hd, N),
+%     maplist(between(1, N), Hd),
+%     plain_within_domain(N, Tl).
+
+plain_get_element([C|R], E, M) :-
+    nth(R, M, Row), nth(C, Row, E).
+
 plain_range(N, L) :-
     findall(X, between(1, N, X), L).
 
 plain_within_domain(N, E) :-
     plain_range(N, L),
-    member(E, L).   
+    member(E, L).
+
+plain_len_row(N, M) :-
+    length(M, N).
 
 plain_all_unique(L) :-
-    length(L, Length),
-    sort(L, L_ordered),
-    length(L_ordered, Length_ordered),
-    Length == Length_ordered.
+    length(L, L1),
+    sort(L, L_sorted),
+    length(L_sorted, L2),
+    (L1 == L2).
 
-plain_all_different(N, L) :-
-    maplist(plain_within_domain(N), L),
-    plain_all_unique(L).
+plain_all_different(N, M) :-
+    maplist(plain_within_domain(N), M),
+    plain_all_unique(M).
+
 
 plain_check_sum_helper(0, [], _).
 plain_check_sum_helper(Sum, [Hd|Tl], M) :-
-    get_element(Hd, E, M),
+    plain_get_element(Hd, E, M),
     Rem is (Sum - E),
-    check_sum_helper(Rem, Tl, M).
+    plain_check_sum_helper(Rem, Tl, M).
 
 plain_check_mult_helper(1.0, [], _).
 plain_check_mult_helper(Product, [Hd|Tl], M) :-
-    get_element(Hd, E, M),
+    plain_get_element(Hd, E, M),
     Rem is (Product / E),
-    check_mult_helper(Rem, Tl, M).
+    plain_check_mult_helper(Rem, Tl, M).
 
 plain_check_constrain(M, +(S, L)) :-
-    check_sum_helper(S, L, M).
+    plain_check_sum_helper(S, L, M).
 
 plain_check_constrain(M, *(P, L)) :-
-    check_mult_helper(P, L, M).
+    plain_check_mult_helper(P, L, M).
 
 plain_check_constrain(M, -(D, J, K)) :-
-    get_element(J, E1, M), get_element(K, E2, M),
+    plain_get_element(J, E1, M), plain_get_element(K, E2, M),
     ((D is (E1 - E2)); (D is (E2 - E1))).
 
 plain_check_constrain(M, /(Q, J, K)) :-
-    get_element(J, E1, M), get_element(K, E2, M),
+    plain_get_element(J, E1, M), plain_get_element(K, E2, M),
     ((E1 is (Q * E2)); (E2 is (Q * E1))).
 
 plain_kenken(N, C, T) :-
-    len_row(T, N),
-    len_col(T, N),
+    plain_len_row(N, T),
+    maplist(plain_len_row(N), T),
     transpose(T, X),
     maplist(plain_all_different(N), T),
     maplist(plain_all_different(N), X),
-    maplist(plain_check_constrain(T), C).
+    maplist(plain_check_constrain(X), C).
+
+
+within_domain_2(N, Domain) :- 
+    findall(X, between(1, N, X), Domain).
+
+fill_2d([], _).
+fill_2d([Head | Tail], N) :-
+    within_domain_2(N, Domain),
+    permutation(Domain, Head),
+    fill_2d(Tail, N).
+
+create_grid(Grid, N) :-
+    length(Grid, N),
+    transpose(Grid, T),
+    maplist(unique_list2(N), T),
+    fill_2d(Grid, N).
+
+
+unique_list2(N, L) :-
+    unique_list1(L, N).
+
+unique_list1(List, N) :-
+	length(List, N),
+	elements_between(List, 1, N),
+	all_unique(List).
+
+elements_between(List, Min, Max) :-
+	maplist(between(Min,Max), List).
+
+within_domain_3(Min, Max, L) :-
+    maplist(between(Min, Max), L).
+
+all_unique([]).
+all_unique([H|T]) :- exists(H, T), !, fail.
+all_unique([H|T]) :- all_unique(T).
+
+exists(X, [X|_]).
+exists(X, [_|T]) :-
+	exists(X, T).
 
 
 
